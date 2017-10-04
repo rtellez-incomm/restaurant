6ba1fa02b92ae122b5c87702d0c75e7bcca03c53
rtellez <rtellez@incomm.com>
2017-10-04 12:18:41 -0400

    Ingredients injected to shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,7 @@ import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-  ingredientChanged = new EventEmitter<ingredient[]>();
+  ingredientChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -17,6 +17,14 @@ export class ShoppingListService {
     this.ingredientChanged.emit(this.ingredients.slice());
   }
 
+  addIngredients(ingredients: Ingredient[]) {
+//    for (let ingredient of ingredients) {
+//      this.addIngredient(ingredient);
+//    }
+    this.ingredients.push(...ingredients); //... aka spread operator allows the push method to pass the ingredients as individual objects not as the whole array object like [] would (array into a list)
+    this.ingredientChanged.emit(this.ingredients.slice());
 
+  }
 
 }
+
