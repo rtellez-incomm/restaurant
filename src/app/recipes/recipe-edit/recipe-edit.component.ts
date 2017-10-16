8b0e6d5fb6a6853d16332a6a4d2bb1a3aebe61a1
rtellez <rtellez@incomm.com>
2017-10-16 06:30:01 -0400

    submitting the recipe edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -30,11 +30,11 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit(){
-    const newRecipe = new Recipe(this.recipeForm.value['name'], this.recipeForm.value['description'], this.recipeForm.value['imagePath'], this.recipeForm.value['ingredients']);
+    // const newRecipe = new Recipe(this.recipeForm.value['name'], this.recipeForm.value['description'], this.recipeForm.value['imagePath'], this.recipeForm.value['ingredients']);
     if (this.editMode) {
-      this.recipeService.updateRecipe(this.id, newRecipe);
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
     } else {
-        this.recipeService.addRecipe();
+        this.recipeService.addRecipe(this.recipeForm.value);
     }
   }
 
