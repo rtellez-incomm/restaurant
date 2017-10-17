2fda7c406d7aa0b7ca8b934d0105bbc5732f754e
rtellez <rtellez@incomm.com>
2017-10-17 09:31:47 -0400

    sending the token with getToken

--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth/auth.service';
 import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
@@ -6,14 +7,17 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {}
+    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
-        return this.http.put('https://ng-recipebook-f45c3.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+        const token = this.authService.getToken();
+        return this.http.put('https://ng-recipebook-f45c3.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipebook-f45c3.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+
+        this.http.get('https://ng-recipebook-f45c3.firebaseio.com/recipes.json?auth=' + token)
         .map(
             (response: Response) => {
                 const recipes: Recipe[] = response.json();
